621c3088aaf120ca72b91da1fff1d36c36488ab7
drunkod <drunkod@gmail.com>
2026-07-10 03:59:13 +0500

    Include pnpm in Nix local runner

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
             nodejs_24
             openssl
             pkg-config
+            pnpm
             postgresql_17
             python3
             redis
@@ -169,6 +170,7 @@ EOF
             runtimeInputs = with pkgs; [
               corepack
               nodejs_24
+              pnpm
               postgresql_17
               redis
               initLocal
